a4c9ee684e1f512ac2dc2a893380460710b226c3
berenisser <berenisser@gmail.com>
2017-05-15 22:19:43 -0400

    Password validated

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ function validateForm(){
 	nombre();
 	apellido();
 	correo();
+	contrasena()
 }
 
 
@@ -17,6 +18,7 @@ function nombre(){
 		}
 }
 
+//validacion apellido
 function apellido(){
 		var apellidoValue = document.getElementById("lastname").value;
 		if (!(/^[A-Z][a-z]{3,19}$/).test(apellidoValue)){
@@ -28,12 +30,25 @@ function apellido(){
 		}
 }
 
+//validacion correo 
 function correo(){
 		var emailValue = document.getElementById("input-email").value;
 		if (!(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/).test(emailValue)){
 		var spancito = document.createElement("span");
 		var texto = document.createTextNode("Correo inválido. Debe tener formato valido.");
 		var clasePadre = document.getElementsByClassName("email-container input-box")[0];
+		spancito.appendChild(texto);
+		clasePadre.appendChild(spancito);
+		}
+}
+
+//validacion contraseña, debe ser mayor de 6 caracteres, y no incluir 123456, 098765 ó password
+function contrasena(){
+		var passValue = document.getElementById("input-password").value;
+		if (passValue=="123456" || passValue=="098765" || passValue=="password" || !(/^(?=.*\d).{6,}$/).test(passValue)){
+		var spancito = document.createElement("span");
+		var texto = document.createTextNode("Contraseña inválida. Debe tener un mínimo de 6 caracteres.");
+		var clasePadre = document.getElementsByClassName("form-group input-box")[0];
 		spancito.appendChild(texto);
 		clasePadre.appendChild(spancito);
 		}
